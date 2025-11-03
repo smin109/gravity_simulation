ef02a5fa0328a6423dc2958c25e58d74c53776f6
tmdals109 <2005choism@kookmin.ac.kr>
2025-11-03 15:42:52 +0900

    header files

---
+++ include/nbody/body.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "vec2.hpp"
+
+struct Body {
+    double m{1.0};
+    Vec2 x{}, v{}, a{};
+};
---
+++ include/nbody/sim.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <random>
+#include "body.hpp"
+
+struct SimParams {
+    int N{200};
+    double G{1.0};
+    double soft2{1e-2};
+    double dt{1e-2};
+    int steps{20000};
+    int saveEvery{10};
+    double boxR{50.0};
+    uint64_t seed{42};
+};
+
+struct simState {
+    std::vector < Body > boides;
+    double t{0.0};
+};
