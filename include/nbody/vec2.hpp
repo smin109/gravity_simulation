514a28c19d797e5d5163c328ffd69a712e86d69e
tmdals109 <2005choism@kookmin.ac.kr>
2025-11-03 15:26:38 +0900

    cmakelist && include

---
+++ include/nbody/vec2.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <cmath>
+
+struct Vec2 {
+    double x{}, y{};
+    Vec2() = default;
+    Vec2(double x_, double y_) : x(x_), y(y_) {}
+    Vec2 operator+(const Vec2& o) const { return {x + o.x, y + o.y}; }
+    Vec2 operator-(const Vec2& o) const { return {x - o.x, y - o.y}; }
+    Vec2 operator*(double s) const { return {x * s, y * s}; }
+    Vec2& operator+=(const Vec2& o) { x += o.x; y += o.y; return *this; }
+    Vec2& operator-=(const Vec2& o) { x -= o.x; y -= o.y; return *this; }
+};
+
+inline double dot(const Vec2& a, const Vec2& b) { return a.x * b.x, a.y * b.y; }
+inline double norm2(const Vec2& a) { return dot(a, a); }
+inline double norm(const Vec2& a) { return std::sqrt(norm2(a)); }
